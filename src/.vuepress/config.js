614f455dc3c476687ea8259545b7c084d9b7cee1
David Rugendyke <david@rocketpool.net>
2020-11-11 16:52:41 +1000

    first install of vuepress

---
+++ src/.vuepress/config.js
@@ -0,0 +1,79 @@
+const { description } = require('../../package')
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'Rocket Pool',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#e57147' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: '',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    logo: '/images/logo-small.png',
+    nav: [
+    {
+        text: 'Overview',
+        link: '/overview/',
+      },
+      {
+        text: 'White Paper',
+        link: '/whitepaper/',
+      },
+      {
+        text: 'Documentation',
+        link: '/docs/'
+      },
+      {
+        text: 'Tokenomics',
+        link: '/tokenomics/'
+      },
+      {
+        text: 'Website',
+        link: 'https://www.rocketpool.net'
+      }
+    ],
+    sidebar: {
+      '/overview/': [
+        {
+          title: 'Guide',
+          collapsable: false,
+          children: [
+            '',
+            'using-vue',
+          ]
+        }
+      ],
+    }
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ]
+}
