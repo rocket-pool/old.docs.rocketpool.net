2b96bf14f36e1e2c322dddccd0f07156a61e85e6
Nick Doherty <nick@nickdoherty.com>
2022-12-21 17:20:22 +1000

    deleted whitepaper and created a redirect to the explainer series

--- src/.vuepress/enhanceApp.js
+++ src/.vuepress/enhanceApp.js
@@ -10,5 +10,8 @@ export default ({
   router, // the router instance for the app
   siteData // site metadata
 }) => {
-  // ...apply enhancements for the site.
+  router.addRoutes([
+    { path: '/whitepaper', redirect: '/overview/explainer-series/' },
+  ])
 }
+
